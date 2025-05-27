0c823402c0d6b08c9896af85b6b520de195c4ef6
Nia <nia@niaswift.com>
2025-05-27 08:05:14 +0300

    Initial commit

---
+++ src/main/java/com/niaswift/clientsided/Clientsided.java
@@ -0,0 +1,17 @@
+package com.niaswift.clientsided;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Clientsided implements ModInitializer {
+
+	public static final String MOD_ID = "clientsided";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/niaswift/clientsided/ClientsidedClient.java
@@ -0,0 +1,10 @@
+package com.niaswift.clientsided;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientsidedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
