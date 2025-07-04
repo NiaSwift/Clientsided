b3f425ad2b298104c4d7cccea9778d0f0fc8bd30
Nia <nia@niaswift.com>
2025-07-04 14:04:07 +0300

    Added keybind

--- src/main/java/com/niaswift/clientsided/ClientsidedClient.java
+++ src/main/java/com/niaswift/clientsided/ClientsidedClient.java
@@ -1,10 +1,33 @@
 package com.niaswift.clientsided;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 
 public class ClientsidedClient implements ClientModInitializer {
+
+    private static KeyBinding keyBinding;
+
     @Override
     public void onInitializeClient() {
 
+        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.clientsided.test", // The translation key of the keybinding's name
+                InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+                GLFW.GLFW_KEY_V, // The keycode of the key
+                "key.categories.creative" // The translation key of the keybinding's category.
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (keyBinding.wasPressed()) {
+                client.player.sendMessage(Text.literal("Key 1 was pressed!"), false);
+            }
+        });
+
     }
+
 }
