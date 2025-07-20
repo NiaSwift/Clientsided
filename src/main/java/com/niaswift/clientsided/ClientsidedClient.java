31ef655827f28aeb45fbbbf8bc9986503210a5dc
Nia <nia@niaswift.com>
2025-07-20 13:36:20 +0300

    Added HUD

--- src/main/java/com/niaswift/clientsided/ClientsidedClient.java
+++ src/main/java/com/niaswift/clientsided/ClientsidedClient.java
@@ -3,9 +3,14 @@ package com.niaswift.clientsided;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.item.Items;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class ClientsidedClient implements ClientModInitializer {
@@ -23,10 +28,33 @@ public class ClientsidedClient implements ClientModInitializer {
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (keyBinding.wasPressed()) {
-                client.player.sendMessage(Text.literal("Key 1 was pressed!"), false);
+            if ( client.player != null ) {
+                while (keyBinding.wasPressed()) {
+                    client.player.sendMessage(Text.literal("Key 1 was pressed!"), false);
+                }
             }
         });
+
+
+        HudElementRegistry.addFirst(
+                Identifier.of(Clientsided.MOD_ID),
+                (context, tickCounter) -> {
+                    ClientPlayerEntity player = MinecraftClient.getInstance().player;
+                    if (
+                            player != null
+                                    && player.getHealth() > 10
+                    ) {
+                        int diamondCount = player.getInventory().count(Items.DIAMOND);
+                        context.drawText(
+                                MinecraftClient.getInstance().textRenderer,
+                                "Diamonds in your inventory: " + diamondCount,
+                                5,
+                                527,
+                                0xFFFFFFFF,
+                                true
+                        );
+                    }
+                });
 
     }
 
